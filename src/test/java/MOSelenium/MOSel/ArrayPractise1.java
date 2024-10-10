c165b6342add04078e1a5c8005184cc8ac6ea39f
Rushali Seth <seth.rushali12@gmail.com>
2024-10-10 11:40:22 -0400

    arrays

---
+++ src/test/java/MOSelenium/MOSel/ArrayPractise1.java
@@ -0,0 +1,16 @@
+package MOSelenium.MOSel;
+
+import java.util.HashMap;
+
+public class ArrayPractise1 {
+    public static void main(String[] args){
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+      
+
+
+
+
+    }
+
+}
--- src/test/java/org/java/practice/ArrayPractise.java
+++ src/test/java/org/java/practice/ArrayPractise.java
@@ -1,6 +1,7 @@
 package org.java.practice;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class ArrayPractise {
 
@@ -21,8 +22,29 @@ public class ArrayPractise {
 
       }
     }
+
+    public static void elementFrequencyInArray(int[] arr){
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i=0;i<arr.length; i++ ){
+            if(hm.containsKey(arr[i])){
+                hm.put(arr[i], hm.get(arr[i]) + 1);
+            }
+            else{
+                hm.put(arr[i], 1);
+            }
+
+        }
+       System.out.println( hm.entrySet());
+
+
+
+
+
+    }
     public static void main(String[] args){
-        System.out.println(isAnagram("ridver", "drier"));
+      //  System.out.println(isAnagram("ridver", "drier"));
+        int[]  a = {2,3,4,5,6,6,6};
+        elementFrequencyInArray(a);
 
     }
 }
